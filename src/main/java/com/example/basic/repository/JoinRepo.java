f701d65091307a898116d60b7e1546a869bc5f70
sangdo14 <sangdo14@naver.com>
2025-05-01 19:12:10 +0900

    수정1

--- src/main/java/com/example/basic/repository/JoinRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.basic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JoinRepo extends JpaRepository<JoinEntity, Long> {
-    JoinEntity findByUnameAndEmail(String uname, String email);
-}
